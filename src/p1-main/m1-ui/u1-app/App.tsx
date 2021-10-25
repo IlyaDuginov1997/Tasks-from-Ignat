143e7d0de9cfcea870115d37c9d38b55098e12e9
Ilya <IlyaDuginov1997@gmail.com>
2021-10-25 20:38:02 +0300

    Task 11

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import s from './App.module.css';
 import HW11 from '../../../p2-homeworks/h11/HW11';
+import HW1 from '../../../p2-homeworks/h1/HW1';
+import HW2 from '../../../p2-homeworks/h2/HW2';
+import HW3 from '../../../p2-homeworks/h3/HW3';
+import HW4 from '../../../p2-homeworks/h4/HW4';
+import HW5 from '../../../p2-homeworks/h5/HW5';
+import HW6 from '../../../p2-homeworks/h6/HW6';
+import HW7 from '../../../p2-homeworks/h7/HW7';
+import HW8 from '../../../p2-homeworks/h8/HW8';
+import HW9 from '../../../p2-homeworks/h9/HW9';
+import HW10 from '../../../p2-homeworks/h10/HW10';
 
 function App() {
     return (
--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,40 +1,59 @@
 import React, {useState} from 'react';
-import Slider, { Range } from 'rc-slider';
+import Slider, {Range} from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import styles from './HW11.module.css'
+import styles from './HW11.module.css';
+
 
 function HW11() {
-    const [value1, setValue1] = useState(0);
-    const [value2, setValue2] = useState(100);
+    const [value1, setValue1] = useState(20);
+    const [value2, setValue2] = useState(80);
+
+    const onChangeSliderValue = (value: number) => {
+        const valueMin = Math.min(Number(value), value2);
+        setValue1(valueMin);
+        console.log(value);
+    };
+
+    const onChangeRangeValue = (values: number[]) => {
+        setValue1(values[0]);
+        setValue2(values[1]);
+    };
+
 
     return (
-        <div className={styles.main}>
+        <div>
             <hr/>
-            homeworks 11
-            {/*<Slider min={0} max={100}/>*/}
-            {/*<Range/>*/}
-            {/*should work (должно работать)*/}
-            {/*<div>*/}
-            {/*    <span>{value1}</span>*/}
-            {/*    <SuperRange*/}
-            {/*        // сделать так чтоб value1 изменялось*/}
-            {/*    />*/}
-            {/*</div>*/}
 
-            {/*<div>*/}
-            {/*    <span>{value1}</span>*/}
-            {/*    <SuperDoubleRange*/}
-            {/*        // сделать так чтоб value1 и value2 изменялось*/}
-            {/*    />*/}
-            {/*    <span>{value2}</span>*/}
-            {/*</div>*/}
+            <div className={styles.mainContainer}>
 
+                homeworks 11
+                <span>{value1}</span>
+                <Slider
+                    onChange={onChangeSliderValue}
+                    min={0}
+                    max={100}
+                    value={value1}
+                />
+
+                <span>{value1}</span>
+                <Range
+                    min={0}
+                    max={100}
+                    onChange={onChangeRangeValue}
+                    value={[value1, value2]}
+                    allowCross={false}/>
+                <span>{value2}</span>
+
+
+                <hr/>
+                {/*для личного творчества, могу проверить*/}
+                {/*<AlternativeSuperRange/>*/}
+                {/*<AlternativeSuperDoubleRange/>*/}
+            </div>
             <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+
         </div>
+
     );
 }
 
